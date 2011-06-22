f5127af9719e97f8e50c6b35fb90121cca858663
Alexey Vasilyev <alexa.infra@gmail.com>
2011-06-23 00:06:15 +0700

    clean up

--- app/demo.cpp
+++ app/demo.cpp
@@ -79,30 +79,16 @@ GlutSampleWindow::GlutSampleWindow(i32 width, i32 height)
     program_ = base::opengl::Program::Create(read_file("shader.vs"), read_file("shader.ps"));
     if (program_ != NULL)
         program_->Bind();
-
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
 }
 
 GlutSampleWindow::~GlutSampleWindow() {
     delete texture_;
+    delete program_;
 }
 
 void GlutSampleWindow::OnDisplay(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-/*    float ambient_and_diffuse [] = { 0.3f, 0.3f, 0.3f, 1.0f };
-    float specular[] = { 0.8f, 0.8f, 0.8f, 1.f };
-    float emission[] = { 0.f, 0.f, 0.f, 1.f };
-
-    glMaterialfv( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, ambient_and_diffuse );
-    glMaterialfv( GL_FRONT_AND_BACK, GL_SPECULAR, specular );
-    glMaterialfv( GL_FRONT_AND_BACK, GL_EMISSION, emission );
-    glMateriali( GL_FRONT_AND_BACK, GL_SHININESS, 64 );*/
-
-//    glBindTexture(GL_TEXTURE_2D, texture_->id());
-
-//    glutSolidTeapot(1.f);
     i32 pos = program_->get_attributes()["position"].Location;
     i32 tex = program_->get_attributes()["tex"].Location;
     i32 color = program_->get_attributes()["color"].Location;
