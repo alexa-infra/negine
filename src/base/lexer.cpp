9d57455b003114c9dde1807adf848f261301a368
Alexey Vasilyev <alexa.infra@gmail.com>
2012-03-31 18:20:58 +0700

    fix identation

--- src/base/lexer.cpp
+++ src/base/lexer.cpp
@@ -12,136 +12,136 @@
 namespace base {
 
 
-    Lexer::Lexer(const std::string &filename)
-        : file_(filename)
-        , maxTokenSize(256) 
-    {
-        token = new char[maxTokenSize];
-        whiteCharacters = new char[128];
-        memset(whiteCharacters, 0, 128);
+Lexer::Lexer(const std::string &filename)
+    : file_(filename)
+    , maxTokenSize(256) 
+{
+    token = new char[maxTokenSize];
+    whiteCharacters = new char[128];
+    memset(whiteCharacters, 0, 128);
 
-        whiteCharacters[' '] = 1;
-        whiteCharacters['\t'] = 1;
-        whiteCharacters[','] = 1;
-        whiteCharacters['('] = 1;
-        whiteCharacters[')'] = 1;
-        whiteCharacters['\r'] = 1;
-        whiteCharacters['\n'] = 1;
-        whiteCharacters['*'] = 1;
-        whiteCharacters[':'] = 1;
+    whiteCharacters[' '] = 1;
+    whiteCharacters['\t'] = 1;
+    whiteCharacters[','] = 1;
+    whiteCharacters['('] = 1;
+    whiteCharacters[')'] = 1;
+    whiteCharacters['\r'] = 1;
+    whiteCharacters['\n'] = 1;
+    whiteCharacters['*'] = 1;
+    whiteCharacters[':'] = 1;
 
-    }
-    Lexer::~Lexer(){
-        delete[] token;
-        delete[] whiteCharacters;
-    }
+}
+Lexer::~Lexer(){
+    delete[] token;
+    delete[] whiteCharacters;
+}
 
-    void Lexer::SkipWhiteSpace() {
-        
-        if (!HasMoreData())
-            return;  
+void Lexer::SkipWhiteSpace() {
     
-        while(HasMoreData() && IsWhiteChar(*token))
+    if (!HasMoreData())
+        return;  
+
+    while(HasMoreData() && IsWhiteChar(*token))
+    {
+        char previousChar = file_.read_type<char>();
+
+        char ch = file_.read_type<char>();
+        file_.set_position(file_.position()-1);
+
+        if (ch == '#')
         {
-            char previousChar = file_.read_type<char>();
-
-            char ch = file_.read_type<char>();
-            file_.set_position(file_.position()-1);
-
-            if (ch == '#')
+            SkipRestOfLine();
+        }
+        
+        if (ch == '/')
+        {
+            if(previousChar == '/')
             {
                 SkipRestOfLine();
-            }
-            
-            if (ch == '/')
+                SkipWhiteSpace();
+                return;
+            }            
+        }
+        
+        if (ch == '*')
+        {
+            if(previousChar == '/')
             {
-                if(previousChar == '/')
+                //Skip until */ is found
+                while (HasMoreData())
                 {
-                    SkipRestOfLine();
+                    previousChar = file_.read_type<char>();
+
+                   ch = file_.read_type<char>();
+                   file_.set_position(file_.position()-1);
+
+                    if ((ch == '/') && (previousChar == '*'))
+                        break;
+                }
+                
+                if ( HasMoreData() )
+                {
+                    previousChar = file_.read_type<char>();
                     SkipWhiteSpace();
-                    return;
-                }            
-            }
-            
-            if (ch == '*')
-            {
-                if(previousChar == '/')
-                {
-                    //Skip until */ is found
-                    while (HasMoreData())
-                    {
-                        previousChar = file_.read_type<char>();
-
-                       ch = file_.read_type<char>();
-                       file_.set_position(file_.position()-1);
-
-                        if ((ch == '/') && (previousChar == '*'))
-                            break;
-                    }
-                    
-                    if ( HasMoreData() )
-                    {
-                        previousChar = file_.read_type<char>();
-                        SkipWhiteSpace();
-                    }
                 }
             }
         }
     }
+}
 
-    void Lexer::SkipRestOfLine() {
-        while (HasMoreData()) {
-            if (file_.read_type<char>() == '\n')
-                break;     
-        }
+void Lexer::SkipRestOfLine() {
+    while (HasMoreData()) {
+        if (file_.read_type<char>() == '\n')
+            break;     
     }
+}
 
 
-    string Lexer::ReadToken() {
-        
-        SkipWhiteSpace();
+string Lexer::ReadToken() {
+    
+    SkipWhiteSpace();
 
-        tokenChar = token;
-        *tokenChar = '\0';
+    tokenChar = token;
+    *tokenChar = '\0';
 
-        if (file_.read_type<char>() == '"') 
+    if (file_.read_type<char>() == '"') 
+    {
+        *tokenChar++ = file_.read_type<char>();
+        while(HasMoreData() && *tokenChar != '"')
         {
             *tokenChar++ = file_.read_type<char>();
-            while(HasMoreData() && *tokenChar != '"')
-            {
-                *tokenChar++ = file_.read_type<char>();
-            }
-
-            *tokenChar++ = '\0';
-            return token;
-        }
-        else 
-        {
-            while(HasMoreData() && !IsWhiteChar(*tokenChar))
-                *tokenChar++ = file_.read_type<char>();
         }
 
         *tokenChar++ = '\0';
-
         return token;
     }
-
-    bool Lexer::IsWhiteChar(char character) {
-        return whiteCharacters[character] == 1;
+    else 
+    {
+        while(HasMoreData() && !IsWhiteChar(*tokenChar))
+            *tokenChar++ = file_.read_type<char>();
     }
 
-    f32 Lexer::ReadReal() {
-        ReadToken(); 
-        return (float)atof(token);
-    }
+    *tokenChar++ = '\0';
 
-    i32 Lexer::HasMoreData() {
-        
-        return (file_.position() <= file_.size());
-    }
-    void   Lexer::SetWhiteCharValue(char c, char value) {
-        
-        whiteCharacters[c] = value;
-    }
+    return token;
+}
+
+bool Lexer::IsWhiteChar(char character) {
+    return whiteCharacters[character] == 1;
+}
+
+f32 Lexer::ReadReal() {
+    ReadToken(); 
+    return (float)atof(token);
+}
+
+i32 Lexer::HasMoreData() {
     
+    return (file_.position() <= file_.size());
+}
+void   Lexer::SetWhiteCharValue(char c, char value) {
+    
+    whiteCharacters[c] = value;
+}
+
 }
