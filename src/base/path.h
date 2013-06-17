91de0d2f8e6043b32797f7b57cf82bf65849f757
Alexey Vasilyev <alexa.infra@gmail.com>
2013-06-17 18:50:49 +0200

    basic operations with path

---
+++ src/base/path.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+
+namespace base { namespace path {
+
+std::string toDir(const std::string& p);
+
+std::string basename(const std::string& p);
+
+std::string dirname(const std::string& p);
+
+std::tuple<std::string, std::string> split(const std::string& p);
+
+std::tuple<std::string, std::string> splitext(const std::string& p);
+
+std::string normpath(const std::string& p);
+
+inline std::string join(const std::string& a) {
+    return a;
+}
+
+template<typename T, typename... Args>
+std::string join(const std::string& a, const T& b, Args... p) {
+    return join(a + '/' + b, p...);
+}
+
+}}
