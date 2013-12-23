a512058d27950c5b0ab5fe8bfc385b4b1cc078dc
Alexey Vasilyev <alexa.infra@gmail.com>
2013-12-23 11:52:08 +0100

    added camera component

---
+++ src/game/components/camera.cpp
@@ -0,0 +1,72 @@
+#include "camera.h"
+#include "transform.h"
+
+namespace base {
+namespace game {
+
+using namespace math;
+
+CompType Camera::type_ = ComponentBase::registerType();
+
+Camera::Camera() {
+    dirty_ = true;
+}
+
+void Camera::setPerspective(f32 aspect, f32 fov, f32 zNear, f32 zFar) {
+    aspect_ = aspect;
+    fov_ = fov;
+    zNear_ = zNear;
+    zFar_ = zFar;
+    dirty_ = true;
+}
+
+void Camera::setAspect( f32 v ) {
+    aspect_ = v;
+    dirty_ = true;
+}
+
+void Camera::setFov( f32 v ) {
+    fov_ = v;
+    dirty_ = true;
+};
+
+void Camera::setZNear( f32 v ) {
+    zNear_ = v;
+    dirty_ = true;
+}
+    
+void Camera::setZFar( f32 v ) {
+    zFar_ = v;
+    dirty_ = true;
+}
+
+void Camera::update() {
+    ComponentBase* component = get_parent(parent_, Transform::Type());
+    Transform* parentTransform = dynamic_cast<Transform*>(component);
+
+    const math::Matrix4& newWorld = parentTransform->world();
+    if (!dirty_ && newWorld == world_)
+        return;
+    dirty_ = false;
+    world_ = newWorld;
+
+    projection_ = Matrix4::Perspective( fov_, aspect_, zNear_, zFar_ );
+    modelview_ = OrthoInverse(world_);
+        //Matrix4::LookAt( position, position + parentTransform->forward(), parentTransform->up() );
+    clip_ = projection_ * modelview_;
+
+    const Matrix4& m = clip_;
+    const math::vec4f v1 = m.Row( 0 );
+    const math::vec4f v2 = m.Row( 1 );
+    const math::vec4f v3 = m.Row( 2 );
+    const math::vec4f v4 = m.Row( 3 );
+    planes_[0].set( v4 + v1 );
+    planes_[1].set( v4 - v1 );
+    planes_[2].set( v4 + v2 );
+    planes_[3].set( v4 - v2 );
+    planes_[4].set( v4 + v3 );
+    planes_[5].set( v4 - v3 );
+}
+
+}
+}
---
+++ src/game/components/camera.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "game/component.h"
+#include "math/matrix.h"
+#include "math/plane.h"
+
+namespace base {
+namespace game {
+
+class Camera : public Component<Camera>
+{
+public:
+    Camera();
+
+    inline f32 aspect() const { return aspect_; }
+    inline f32 fov() const { return fov_; }
+    inline f32 zNear() const { return zNear_; }
+    inline f32 zFar() const { return zFar_; }
+
+    inline const math::Plane* planes() const { return planes_; }
+    inline const math::Matrix4& projection() const { return projection_; }
+    inline const math::Matrix4& modelView() const { return modelview_; }
+    inline const math::Matrix4& clipMatrix() const { return clip_; }
+
+    void setPerspective(f32 aspect, f32 fov, f32 zNear, f32 zFar);
+    void setAspect(f32 ratio);
+    void setFov(f32 radians);
+    void setZNear(f32 dist);
+    void setZFar(f32 dist);
+private:
+    void update();
+
+    bool dirty_;
+    math::Plane planes_[6];
+    math::Matrix4 projection_;
+    math::Matrix4 modelview_;
+    math::Matrix4 clip_;
+    math::Matrix4 world_;
+
+    f32 aspect_;
+    f32 fov_;
+    f32 zNear_;
+    f32 zFar_;
+};
+
+} // namespace game
+} // namespace base
